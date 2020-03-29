901f934ed694a5fa01909ef07b29bed2d974fb9b
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-03-29 15:10:44 +0200

    Update pp.py

---
+++ python/pp.py
@@ -0,0 +1,22 @@
+from bottle import route, run, template, static_file, request, redirect
+from os import listdir
+
+import psycopg2
+
+#connect to the db
+con = psycopg2.connect( 
+    dbname="padel", 
+    user="ak3672",
+    password="294evcub",
+    host="pgserver.mah.se")
+
+cur = con.cursor()
+
+@route('/')
+def index():
+    cur.execute('select namn from person')
+    namn = cur
+    return template('index.html')
+
+run(host='localhost', port=8080, debug=True)
+con.close()
