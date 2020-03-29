dca8ecc25f56f09fb6f738c6a1de4c60ad16bc30
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-03-29 14:08:26 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -7,18 +7,18 @@ pnr integer,
 namn char(30), 
 level integer,
 info char(100)
-PRIMARY KEY pnr
+PRIMARY KEY (pnr)
 );
 
 create table ort (
 ortID integer,
 stad char(40)
-PRIMARY KEY ortID
+PRIMARY KEY (ortID)
 );
 
 create table profile (
 id integer,
 username char(50),
 password char(50)
-PRIMARY KEY id
+PRIMARY KEY (id)
 ); 
