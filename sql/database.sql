4aace481633728470f7859880e14c73e09e9a2a1
albin-ux <62427356+albin-ux@users.noreply.github.com>
2020-03-29 14:13:22 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -6,19 +6,19 @@ create table person (
 pnr integer, 
 namn char(30), 
 level integer,
-info char(100)
+info char(100),
 PRIMARY KEY (pnr)
 );
 
 create table ort (
 ortID integer,
-stad char(40)
+stad char(40),
 PRIMARY KEY (ortID)
 );
 
 create table profile (
 id integer,
 username char(50),
-password char(50)
+password char(50),
 PRIMARY KEY (id)
 ); 
