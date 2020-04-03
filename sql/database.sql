a54b7ac2cfdb8599da966b261ce953ec3982f9f9
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2020-04-03 11:30:25 +0200

    updated sql script

--- sql/database.sql
+++ sql/database.sql
@@ -2,13 +2,6 @@ drop database if exits padelpartner;
 
 drop table if exits person;
 
-create table person (
-pnr integer, 
-namn char(30), 
-level integer,
-info char(100),
-PRIMARY KEY (pnr)
-);
 
 create table person (
 pnr BIGINT CONSTRAINT TenDigits CHECK (pnr BETWEEN 1000000000 and 9999999999),
@@ -21,13 +14,20 @@ PRIMARY KEY (pnr)
 
 create table ort (
 ortID integer,
-stad char(40),
+stad varchar(40),
 PRIMARY KEY (ortID)
 );
 
 create table profile (
 id integer,
-username char(50),
-password char(50),
+username varchar(50),
+password varchar(50),
 PRIMARY KEY (id)
 ); 
+
+create table profile1 (
+id integer
+gender varchar (50)
+info varchar (1500)
+PRIMARY KEY (id)
+)
