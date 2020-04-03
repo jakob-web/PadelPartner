221af27cec5b20f38dd28ca446cbed1f1b2b7233
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-04-03 10:12:44 +0200

    changes
    Stoppade in våra moduler i pp.py för att testa om allt fungerade tillsammans samt skapade en profilsida där användaren kan ändra sin profil

--- python/pp.py
+++ python/pp.py
@@ -18,5 +18,36 @@ def index():
     namn = cur.fetchall()
     return template('index.html', namn=namn)
 
+@route('/logIn')
+def logIn():
+
+    return template("log_in.html")
+
+@route('/logInUser', method="POST")
+def logInUser():
+    print("hej")
+    # data regarding the profile table
+    userName = getattr(request.forms,"userName")
+    password = getattr(request.forms,"password")
+    print(userName, password)
+
+    def selectMember():
+        userSelect = []
+        userSelect.append(userName)
+        userSelect.append(password)
+        
+        sql = "select * from profile where username = %s AND profile.password = %s"
+        cur.execute(sql, userSelect)
+        
+
+    selectMember()
+    
+    return template("welcome.html")
+
+@route('/myprofile')
+def myProfile():
+    
+    return template('description.html')
+
 run(host='localhost', port=8080, debug=True)
 con.close()
