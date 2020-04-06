b857552a20b502aa5702c85340c6220e12990583
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-04-06 14:24:17 +0200

    Revert "vet inte"
    This reverts commit 6ee82f11e815c1f4b9653ca5a35b0ef42fba0cfe.

--- python/user_login.py
+++ python/user_login.py
@@ -11,9 +11,29 @@ con = psycopg2.connect(
 
 cur = con.cursor()
 
+
+<<<<<<< Updated upstream
+
 #current table = profiletest // change this later!
 
+
+
 def password():
+||||||| merged common ancestors
+
+
+"""@route('/logIn')
+def logIn():
+
+    return template("log_in.html", username ="")"""
+
+#current table = profiletest // change this later!
+
+
+def password():
+=======
+def login():
+>>>>>>> Stashed changes
     cred = []
     cur.execute("select username from profile")
     cred = cur.fetchall()
@@ -21,6 +41,7 @@ def password():
     print(usernameList)
     username = getattr(request.forms, "userName")
     password = getattr(request.forms, "pwd")
+<<<<<<< Updated upstream
     for name in cred:
         if username == name[0]:
             cur.execute("select password from profiletest where username='%s'" % (username))
@@ -35,5 +56,44 @@ def password():
         else:
             redirect("/log_in")
 
+Detta är den nya
+
 con.close()
 
+||||||| merged common ancestors
+    for name in cred:
+        if username == name[0]:
+            cur.execute("select password from profiletest where username='%s'" % (username))
+            cred = cur.fetchall()
+            for pwd in cred:
+                if password == pwd[0]:
+                    print("hej")
+                    return template("welcome.html", user = username)
+                else:
+                    print("Felaktigt lösenord eller Användarnamn")
+                    redirect('/log_in')
+        else:
+            redirect('/log_in')
+
+    
+
+
+run(host='localhost', port=8080, debug=True)
+con.close()
+
+=======
+    
+    if username in usernameList:
+        print("YES1")
+        cur.execute("select password from profile where username='%s'" % (username))
+        cred = cur.fetchall()
+        cred = ("".join(str(cred)))
+        if password in cred:
+                print("YES2")
+                return True
+        else:
+            print("fel lösenord")
+            return False
+    else:
+        return False
+>>>>>>> Stashed changes
