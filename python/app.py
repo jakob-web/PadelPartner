7f631e6902eceda2293c0f5258a88243f93b67af
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2020-04-09 14:39:48 +0200

    updated profile

--- python/app.py
+++ python/app.py
@@ -42,7 +42,13 @@ def test():
 @route('/logInUser', method="POST")
 def test2():
     if user_login.login() == True:
-        return template("welcome.html", user = "")
+        username = getattr(request.forms, "userName")
+        cur.execute("select img from(profile join registration on profile.pid = registration.pid) where username = %s", [username])
+        img = cur.fetchone()
+        # img = profile.getImg(username)
+        # pid = "Select pid from registration where username = %s", [username]
+        return template("welcome.html", picture = img, user = username)
+        
     elif user_login.login() == False:
         return template("log_in.html", username = "")
 
