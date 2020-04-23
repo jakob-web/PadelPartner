f1967572dda46692b7d1d3ec2abaa2545bbbb0d9
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-04-23 16:27:22 +0200

    fixat find_match

--- python/show_match.py
+++ python/show_match.py
@@ -4,11 +4,11 @@ from os import listdir
 import psycopg2
 
 con = psycopg2.connect( 
-    dbname="filipspadel", 
-    user="ak1838",
-    password="xrqhw4q4",
+    dbname="tennispartner", 
+    user="ak3672",
+    password="294evcub",
     host="pgserver.mah.se")
-
+    
 cur = con.cursor()
 #TODO BUG, Skapa match och gå sedan ett steg tillbaka så skapas match igen & igen.....
 
