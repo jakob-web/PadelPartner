4213f7a7f1838ede8c3be5d39ffc1a406c1d9dfa
Filip-M <filip.moltzer@gmail.com>
2020-05-05 17:43:03 +0200

    Deleted old SQL script

--- sql/database.sql
+++ sql/database.sql
@@ -1,38 +1,3 @@
-drop database if exits padelpartner;
-
-drop table if exits person;
-
-
-create table person (
-pnr BIGINT CONSTRAINT TenDigits CHECK (pnr BETWEEN 1000000000 and 9999999999),
-namn varchar(100),
-level int,
-info varchar(1000),
-PRIMARY KEY (pnr)
-);
-
-
-create table ort (
-ortID integer,
-stad varchar(40),
-PRIMARY KEY (ortID)
-);
-
-create table profile (
-id integer,
-username varchar(50),
-password varchar(50),
-PRIMARY KEY (id)
-); 
-
-create table profile1 (
-id integer
-gender varchar (50),
-info varchar (1500)
-PRIMARY KEY (id)
-)
-
--- # NEW: 
 create table person (
     pid integer,
     name varchar(100),
