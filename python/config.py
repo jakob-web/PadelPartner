5993b575e1395b90152ef0a5dca95cede64f24dd
marcusasker <55256718+marcusasker@users.noreply.github.com>
2020-05-07 14:06:58 +0200

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 dbname ="padelpartner"
 user ="postgres"
 password ="albin123"
 host ="127.0.0.1"
 UPLOAD_FOLDER = "static"
+=======
+dbname ="padel10"
+user ="postgres"
+password ="postgres"
+host ="127.0.0.1"
+>>>>>>> Stashed changes
