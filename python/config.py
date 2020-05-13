966cd6e37440c31992302e2f1c9790d7a47631cf
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2020-05-13 11:35:24 +0200

    r

--- python/config.py
+++ python/config.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 dbname ="padel9"
 user ="oscarkindvall"
 password ="Magdajava66"
+=======
+dbname ="padelpartner"
+user ="postgres"
+password ="albin123"
+>>>>>>> parent of 4b5ac0f... lade till info im klasser
 host ="127.0.0.1"
